6bdb0b865833094ba83dea67dc4894a30e7cfde8
wulff <wulff@ratatosk.net>
2017-06-09 17:06:35 +0200

    Added basic app setup

---
+++ app/app.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Controller\DefaultController;
+use Monolog\Handler\SyslogHandler;
+use Silex\Application;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+
+// Default configuration. See README.md for a complete example.
+$config = [
+    'debug' => true,
+    'environment' => 'development',
+    'proxies' => [],
+];
+
+if (file_exists(__DIR__ . '/config.php')) {
+    include __DIR__ . '/config.php';
+}
+
+$app = new Application($config);
+
+// Register the Twig template engine service.
+$app->register(new TwigServiceProvider(), [
+    'twig.path' => __DIR__ . '/views',
+]);
+
+// Register the Monolog logging service and send all messages to the syslog.
+$app->register(new MonologServiceProvider(), [
+    'monolog.handler' => new SyslogHandler('middlehen'),
+]);
+
+$app->mount('/', new DefaultController());
+
+return $app;
