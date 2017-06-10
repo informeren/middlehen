2c522484744da2e28a0f00cf4c0b986f4ca8f9b7
wulff <wulff@ratatosk.net>
2017-06-10 21:50:18 +0200

    Support GET requests with service config validation

--- src/App/Controller/ProxyController.php
+++ src/App/Controller/ProxyController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Silex\Application;
 use Silex\Api\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProxyController implements ControllerProviderInterface
 {
@@ -16,14 +18,46 @@ class ProxyController implements ControllerProviderInterface
      */
     public function connect(Application $app)
     {
-        /* @var $controllers \Silex\ControllerCollection */
+        /** @var $controllers \Silex\ControllerCollection */
         $controllers = $app['controllers_factory'];
 
-        // Generic controller for GET requests.
-        $controllers->get('/{endpoint}', function (Application $app, $endpoint) {
-            $proxies = $app['proxies'];
-            return "Endpoint: $endpoint";
-        })->assert('endpoint', '.*');
+        $controllers
+            ->match('/{service}/{endpoint}', function (Request $request, $service, $endpoint) use ($app) {
+                $config = $app['proxies'][$service];
+
+                /** @var $app \GuzzleHttp\Client[] */
+                $client = $app['client'];
+
+                $query = $request->query->all();
+                if (!empty($config['authentication']['query_parameters'])) {
+                    $query = array_merge($query, $config['authentication']['query_parameters']);
+                }
+
+                $options = [
+                    'query' => $query,
+                ];
+
+                $uri = $config['base_url'] . $endpoint;
+
+                $response = $client->request($request->getMethod(), $uri, $options);
+
+                $headers = [
+                    'Cache-Control' => $config['cache_control'], // Only for GET requests
+                    'Content-Type' => $response->getHeader('Content-Type'),
+                ];
+
+                return new Response($response->getBody(), $response->getStatusCode(), $headers);
+            })
+            ->assert('endpoint', '.*')
+            ->before(function (Request $request) use ($app) {
+                // TODO: Only allow proxy requests from trusted IPs
+                $path = substr($request->getPathInfo(), 1);
+                list(, $service, ) = explode('/', $path);
+                if (empty($app['proxies'][$service])) {
+                    return new Response('Unknown service', 400);
+                }
+                return null;
+            });
 
         return $controllers;
     }
