2bca838c50e27628760f768f5b3981d23beab7c2
wulff <wulff@ratatosk.net>
2017-06-10 12:35:49 +0200

    Fixed code style issue

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,7 +5,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = require __DIR__ . '/../app/app.php';
 if ($app instanceof Silex\Application) {
     $app->run();
-}
-else {
+} else {
     echo 'Failed to initialize application.';
 }
