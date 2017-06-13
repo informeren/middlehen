94146553b437a6c18335bc512fb5b64660ab3a40
wulff <wulff@ratatosk.net>
2017-06-13 13:16:14 +0200

    Removed 'before' middleware, moved headers to 'after' middleware

--- src/App/Controller/ProxyController.php
+++ src/App/Controller/ProxyController.php
@@ -21,45 +21,37 @@ class ProxyController implements ControllerProviderInterface
         /** @var $controllers \Silex\ControllerCollection */
         $controllers = $app['controllers_factory'];
 
+        /** @noinspection PhpUnusedParameterInspection */
         $controllers
             ->match('/{service}/{endpoint}', function (Request $request, $service, $endpoint) use ($app) {
+                if (empty($app['proxies'][$service])) {
+                    return new Response('Unknown service', 400);
+                }
+
                 $app['middlehen.config'] = $app['proxies'][$service];
+
+                $options = $app['middlehen.options'];
+                $options->addQueryParameters($request->query->all());
 
                 /** @var $client \GuzzleHttp\Client */
                 $client = $app['middlehen.client'];
-                $options = $app['middlehen.options'];
-
-                $options->addQueryParameters($request->query->all());
-
                 $response = $client->request($request->getMethod(), $endpoint, $options->getOptions());
-
-                $headers = [
-                    'Cache-Control' => $app['middlehen.config']['cache_control'],
-                    'Content-Type' => $response->getHeader('Content-Type'),
-                    'Vary' => 'Accept',
-                ];
 
                 $status_code = $response->getStatusCode();
                 if ($template = $request->headers->get('x-mustache')) {
                     $template = $service . '/' . $template . '.mustache';
-                    $data = json_decode($response->getBody(), true); // ,true?
+                    $data = json_decode($response->getBody());
                     $headers['Content-Type'] = 'text/html';
                     return new Response($app['mustache']->render($template, $data), $status_code, $headers);
                 } else {
+                    $headers['Content-Type'] = $response->getHeader('Content-Type');
                     return new Response($response->getBody(), $status_code, $headers);
                 }
             })
             ->assert('endpoint', '.*')
-            ->before(function (Request $request, Application $app) {
-                $path = substr($request->getPathInfo(), 1);
-                list(, $service, ) = explode('/', $path);
-                if (empty($app['proxies'])) {
-                    return new Response('No proxies configured', 400);
-                }
-                if (empty($app['proxies'][$service])) {
-                    return new Response('Unknown service', 400);
-                }
-                return null;
+            ->after(function (Request $request, Response $response, Application $app) {
+                $response->headers->set('Cache-Control', $app['middlehen.config']['cache_control']);
+                $response->headers->set('Vary', 'Accept');
             });
 
         return $controllers;
